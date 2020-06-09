3936a8f49d5319a7305562e3dba07f980eb5effb
jstywxctlhb <463520202@qq.com>
2020-06-09 10:44:24 +0800

    first commit

---
+++ springboot-easypoi/src/main/java/cn/tellsea/SpringbootEasypoiApplication.java
@@ -0,0 +1,14 @@
+package cn.tellsea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan("cn.tellsea.mapper")
+public class SpringbootEasypoiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEasypoiApplication.class, args);
+    }
+}
